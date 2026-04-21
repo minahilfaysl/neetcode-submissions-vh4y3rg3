8191b746c722252fcc80a22102f4e65582dbbbd7
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-04-20 20:03:25 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.js
@@ -0,0 +1,49 @@
+class TimeMap {
+    constructor() {
+        // key: key,
+        // value: object {
+        //   timestamp: value
+        //   eg: 1: "happy"
+        // }
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        // Storing multiple values for the same key at specified time stamps
+        if (!this.keyStore.has(key)) {
+            this.keyStore.set(key, []);
+        }
+        this.keyStore.get(key).push([timestamp, value]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        // Retrieving the key's value at a specified timestamp
+        const values = this.keyStore.get(key) || [];
+        let left = 0;
+        let right = values.length - 1;
+        let result = '';
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (values[mid][0] <= timestamp) {
+                result = values[mid][1];
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
