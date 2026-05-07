171bbf41803f2bb9fe6136a716625620dc6b3400
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-05-07 15:49:55 -0700

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        if (nums.length === 0) return 0;
+
+        let k = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] !== val) {
+                nums[k] = nums[i]
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
