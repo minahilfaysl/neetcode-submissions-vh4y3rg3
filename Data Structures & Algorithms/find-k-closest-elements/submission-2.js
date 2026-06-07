42f5209bf7859bf4e8b52049e48bdb2f50dface6
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-06-07 14:31:50 -0700

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @param {number} k
+     * @param {number} x
+     * @return {number[]}
+     */
+    findClosestElements(arr, k, x) {
+        let l = 0;
+        let r = arr.length - k;
+
+        while (l < r) {
+            const mid = Math.floor((l + r) / 2);
+
+            if (x - arr[mid] > arr[mid + k] - x) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return arr.slice(l, l + k);
+    }
+}
