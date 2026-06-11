b19aa4a52fb2e9c12ba9314030ae36400beb220f
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-06-11 11:23:45 -0700

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {   
+        function dfs(p, q) {
+            if (!p && !q) return true;
+
+            if (!p || !q || p.val !== q.val) {
+                return false;
+            }
+            
+            return dfs(p.left, q.left) && dfs(p.right, q.right);
+        }
+
+        return dfs(p, q);
+    }
+}
