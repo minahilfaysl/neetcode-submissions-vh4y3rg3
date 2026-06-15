a402b81bb9df02e5dca9a6a6720fe83e3ce37de5
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-06-14 18:43:51 -0700

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    postorderTraversal(root) {
+        // left -> right -> root
+        let result = []
+
+        function dfs(node) {
+            if (!node) return;
+
+            dfs(node.left);
+            dfs(node.right);
+            result.push(node.val);
+        }
+
+        dfs(root);
+        return result;
+    }
+}
