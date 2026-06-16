f8f5590ce66c1512722d0b2635d5fa15a338c57c
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-06-16 11:00:26 -0700

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1, word2) {
+        let l1 = 0;
+        let l2 = 0;
+        let str = "";
+
+        while(l1 < word1.length || l2 < word2.length) {
+            if (l1 < word1.length) str += word1[l1];
+            if (l2 < word2.length) str += word2[l2];
+            l1++;
+            l2++;
+        }
+
+        return str;
+    }
+}
