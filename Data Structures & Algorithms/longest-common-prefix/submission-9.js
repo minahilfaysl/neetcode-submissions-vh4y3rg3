4d2e10392179fed3fdc7f099f2305743c0ada2c2
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-08-16 15:53:45 -0700

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        if (strs.length === 0) return "";
+
+        let baseWord = strs[0];
+        
+        // compare each character of baseword with each str with the character at that position
+        for(let i = 0; i < baseWord.length; i++) {
+            let baseChar = baseWord[i];
+
+            for (let j = 1; j < strs.length; j++) {
+                if (baseChar != strs[j][i]) {
+                    return baseWord.slice(0, i);
+                }
+            }
+        }
+
+        return baseWord;
+    }
+}
