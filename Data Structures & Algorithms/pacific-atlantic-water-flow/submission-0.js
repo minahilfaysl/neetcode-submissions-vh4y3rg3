b5103e16e312342cc6536bf24abdcd86d99e9c5c
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-08-22 15:11:54 -0700

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.js
@@ -0,0 +1,58 @@
+class Solution {
+    /**
+     * @param {number[][]} heights
+     * @return {number[][]}
+     */
+    pacificAtlantic(heights) {
+        let rows = heights.length;
+        let cols = heights[0].length;
+
+        // visited sets
+        let pacific = Array.from({ length: rows }, () => Array(cols).fill(false));
+        let atlantic = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+        function dfs(r, c, ocean) {
+            ocean[r][c] = true;
+            let directions = [
+                [1, 0],
+                [-1, 0],
+                [0, 1],
+                [0, -1],
+            ];
+            for (let [dr, dc] of directions) {
+                let nr = r + dr,
+                    nc = c + dc;
+                if (
+                    nr >= 0 &&
+                    nr < rows &&
+                    nc >= 0 &&
+                    nc < cols &&
+                    !ocean[nr][nc] &&
+                    heights[nr][nc] >= heights[r][c]
+                ) {
+                    dfs(nr, nc, ocean);
+                }
+            }
+        }
+
+        for (let c = 0; c < cols; c++) {
+            dfs(0, c, pacific);
+            dfs(rows - 1, c, atlantic);
+        }
+        for (let r = 0; r < rows; r++) {
+            dfs(r, 0, pacific);
+            dfs(r, cols - 1, atlantic);
+        }
+
+        let res = [];
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (pacific[r][c] && atlantic[r][c]) {
+                    res.push([r, c]);
+                }
+            }
+        }
+        return res;
+
+    }
+}
