5ef2764f8f45507f37e2d5020ff0959c8f32a22b
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-08-28 15:20:53 -0700

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        // we traverse the graph, mark all nodes as visited that we have visited
+        // 1 iteration / traversal could be 1 component
+
+        if (n <= 1) return n;
+
+        // create the adj list
+        let adj = Array.from(
+            { length: n },
+            () => []
+        );
+        for (let [u, v] of edges) {
+            adj[u].push(v);
+            adj[v].push(u);
+        }
+
+        console.log(adj)
+
+        let visited = new Set();
+
+        function dfs(i) {
+            if (visited.has(i)) {
+                return;
+            };
+
+            visited.add(i);
+
+            for (let child of adj[i]) {
+                dfs(child);
+            }
+        }
+
+        let count = 0;
+        for (let i = 0; i < n; i++) {
+            if (!visited.has(i)) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
