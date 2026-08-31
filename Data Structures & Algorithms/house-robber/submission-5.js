f7396172491b0115b036c02de499f0038cdb824a
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-08-30 18:14:06 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        const n = nums.length;
+        const memo = new Array(n).fill(-1);
+
+        function dfs(i) {
+            if (i >= n) return 0;
+
+            if (memo[i] !== -1) return memo[i];
+
+            const rob = nums[i] + dfs(i + 2);
+            const skip = dfs(i + 1);
+
+            memo[i] = Math.max(rob, skip);
+
+            return memo[i];
+        }
+
+        return dfs(0);
+    }
+}
