a0ec523dfc41d5f76a8717d565080385cb0baab8
Minahil Faisal <89455927+minahilfaysl@users.noreply.github.com>
2026-08-31 15:56:30 -0700

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        const n = s.length;
+        const dp = Array.from(
+            { length: n },
+            () => Array(n).fill(false)
+        );
+
+        let count = 0;
+
+        for (let i = n - 1; i >= 0; i--) {
+            for (let j = i; j < n; j++) {
+                if (
+                    s[i] === s[j] &&
+                    (j - i <= 2 || dp[i + 1][j - 1])
+                ) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
